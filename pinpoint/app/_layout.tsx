753f1c9875e1a35cd4dda143ff903214697a7472
Yuna41 <uncnprs@gmail.com>
2026-03-03 09:00:10 -0800

    Create dummy navigation

--- pinpoint/app/_layout.tsx
+++ pinpoint/app/_layout.tsx
@@ -29,14 +29,14 @@ export default function RootLayout() {
 
   return (
   <Stack>
-      {/* <Stack.Screen
-      name="(screen)"
+      <Stack.Screen
+      name="(root)"
       options={{headerShown:false}}
       />
       <Stack.Screen
       name="(auth)"
       options={{headerShown:false}}
-      />*/}
+      />
       <Stack.Screen
       name="index"
       options={{headerShown:false}}
